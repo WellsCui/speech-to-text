08f8f66ce468b8d3c368eb5b0b08057f0b60c098
Wei Cui <wells.cui@gmail.com>
2020-04-19 20:24:16 -0400

    refactor function

--- utils.py
+++ utils.py
@@ -21,6 +21,7 @@ import librosa  # for audio processing
 import re
 from typing import Callable, Any, Iterable
 
+
 def pad_sents_char(sents, char_pad_token):
     """ Pad list of sentences according to the longest sentence in the batch and max_word_length.
     @param sents (list[list[list[int]]]): list of sentences, result of `words2charindices()`
@@ -113,6 +114,7 @@ def read_corpus(file_path, source):
 
     return data
 
+
 def read_corpus_from_LJSpeech(file_path, source, line_num=-1):
     """ Read file, where each sentence is dilineated by a `\n`.
     @param file_path (str): path to file containing corpus
@@ -121,12 +123,12 @@ def read_corpus_from_LJSpeech(file_path, source, line_num=-1):
     @param source (str): "tgt" or "src" indicating whether text
         is of the source language or target language
     """
-    data = {}
+    data = []
     line_count = 0
     for line in open(file_path):
         sent_info = line.split('|')
         voice_name = sent_info[0]
-        sent = re.sub('[,";:]', '', sent_info[-1])\
+        sent = re.sub('[,";:\?\(\)]', '', sent_info[-1])\
             .lower()\
             .replace("-- ", "")\
             .replace("-", " ")\
@@ -135,26 +137,44 @@ def read_corpus_from_LJSpeech(file_path, source, line_num=-1):
             .strip()\
             .split(' ')
         last_char = sent[-1][-1]
-        if  last_char in ['.', ';', ","]:
+        if last_char in ['.', ';', ","]:
             sent[-1] = sent[-1][:-1]
         #     sent = sent + [last_char]
 
         # only append <s> and </s> to the target sentence
         if source == 'tgt':
             sent = ['<s>'] + sent + ['</s>']
-        data[voice_name]=sent
-        line_count+=1
-        if line_count==line_num:
+        yield (voice_name, sent)
+        line_count += 1
+        if line_count == line_num:
             break
-    return data
+
 
 def get_voice_files_and_corpus(voice_path: str, voice_num=-1) -> Tuple[List[str], List[List[str]]]:
     corpus_map = read_corpus_from_LJSpeech(voice_path + '/metadata.csv', 'tgt', voice_num)
     voice_files = []
     corpus = []
-    for voice_file in corpus_map:
+    for voice_file, sent in corpus_map:
         voice_files.append(voice_path+'/'+voice_file+'.wav')
-        corpus.append(corpus_map[voice_file])
+        corpus.append(sent)
+    return voice_files, corpus
+
+
+def get_voice_files_and_corpus_by_indexes(voice_path: str, indexes) -> Tuple[List[str], List[List[str]]]:
+    corpus_map = read_corpus_from_LJSpeech(voice_path + '/metadata.csv', 'tgt')
+    voice_files = []
+    corpus = []
+    index_count = len(indexes)
+    index_pos = 0
+    corpus_pos = 0
+    for voice_file, sent in corpus_map:
+        if indexes[index_pos] == corpus_pos:
+            index_pos += 1
+            voice_files.append(voice_path+'/'+voice_file+'.wav')
+            corpus.append(sent)
+            if index_pos >= index_count:
+                break
+        corpus_pos += 1
     return voice_files, corpus
 
 
@@ -179,6 +199,76 @@ def batch_iter(data, batch_size, shuffle=False):
         tgt_sents = [e[1] for e in examples]
 
         yield src_sents, tgt_sents
+
+
+def batch_iter_to_queue(data, batch_queue, batch_size, shuffle=False):
+    """ Yield batches of source and target sentences reverse sorted by length (largest to smallest).
+    @param data (list of (src_sent, tgt_sent)): list of tuples containing source and target sentence
+    @param batch_size (int): batch size
+    @param shuffle (boolean): whether to randomly shuffle the dataset
+    """
+    print("batch_iter_to_queue started")
+    batch_num = math.ceil(len(data) / batch_size)
+    index_array = list(range(len(data)))
+
+    if shuffle:
+        np.random.shuffle(index_array)
+
+    for i in range(batch_num):
+        indices = index_array[i * batch_size: (i + 1) * batch_size]
+        examples = [data[idx] for idx in indices]
+
+        examples = sorted(examples, key=lambda e: len(e[0]), reverse=True)
+        voice_files = [e[0] for e in examples]
+        voices = load_voices_files(voice_files, sample_rate, resample_rate)
+        tgt_sents = [e[1] for e in examples]
+        batch_queue.put((voices, tgt_sents))
+    batch_queue.put((None, None))
+    print("batch_iter_to_queue ended")
+
+
+def batch_iter_to_queue2(data, batch_queue, epoch_num, cache_repeat_count, cache_size, batch_size, shuffle=False):
+    """ Yield batches of source and target sentences reverse sorted by length (largest to smallest).
+    @param data (list of (src_sent, tgt_sent)): list of tuples containing source and target sentence
+    @param batch_size (int): batch size
+    @param shuffle (boolean): whether to randomly shuffle the dataset
+    """
+    for epoch in range(epoch_num):
+      print("epoch:", epoch, "started")
+      index_array = list(range(len(data)))
+
+      if shuffle:
+          np.random.shuffle(index_array)
+
+      print("data len:", len(data))
+
+      cache_num =  math.ceil(len(data) / cache_size)
+      for cache_idx in range(cache_num):
+          cache_indexes = index_array[cache_idx * cache_size: (cache_idx + 1) * cache_size]
+          print("cache_indexes len:", len(cache_indexes))
+          cache = [data[idx] for idx in cache_indexes]
+
+          cache_voice_files, cache_tgt_sents = get_voice_files_and_corpus_by_indexes('dataset/train', cache)
+          cache_voices = load_voices_files(cache_voice_files, sample_rate, resample_rate)
+
+          
+          cache_index_array = list(range(len(cache)))
+          print("cache_voices len:", len(cache_voices))
+          print("cache_voice_files len:", len(cache_voice_files))
+          for rep in range(cache_repeat_count):
+              
+              if shuffle:
+                np.random.shuffle(cache_index_array)
+              print("epoch:", epoch, "cache:", cache_idx, "repeat:", rep)
+              batch_num = math.ceil(len(cache) / batch_size)
+              for i in range(batch_num):
+                  print("batch index:", (i + 1) * batch_size)
+                  itr_indexes = cache_index_array[i * batch_size: (i + 1) * batch_size]
+                  itr_voices = [cache_voices[bidx] for bidx in itr_indexes]
+                  itr_tgt_sents = [cache_tgt_sents[bidx] for bidx in itr_indexes]
+                  batch_queue.put((epoch, itr_voices, itr_tgt_sents))
+    batch_queue.put((None, None, None))
+
 
 
 def read_voice(voice_file, sample_rate, resample_rate=8000, chunk_size=2048, pad_value=0.0):
@@ -218,6 +308,7 @@ def load_voices_files(voice_files, sample_rate, resample_rate=8000, voice_num=-1
             break
     return voices
 
+
 def split_source_with_pad(source: List[List[float]], chunk_size=2048, max_chunk=40, pad_value=0.0) -> (np.ndarray, List[int]):
     """ split source with pad.
 
@@ -232,10 +323,11 @@ def split_source_with_pad(source: List[List[float]], chunk_size=2048, max_chunk=
         sample_len = len(data)
         chunk_num, left = divmod(sample_len, chunk_size)
         if left == 0:
-            lengths.append(chunk_num) 
+            lengths.append(chunk_num)
         else:
             lengths.append(chunk_num+1)
-        padded_data = np.concatenate((data, [pad_value] * (chunk_size*max_chunk - sample_len)))
+        padded_data = np.concatenate(
+            (data, [pad_value] * (chunk_size*max_chunk - sample_len)))
         chunks = np.split(padded_data, max_chunk)
         splited_source.append(chunks)
     return np.array(splited_source), lengths
@@ -256,7 +348,8 @@ def split_voices_with_pad(voices: List[List[float]], chunk_size=1024, max_chunk=
         chunk_num = len(splited_voice)
         if chunk_num < max_chunk:
             lengths.append(chunk_num)
-            padded_data = splited_voice + [[pad_value]*chunk_size]*(max_chunk - chunk_num)
+            padded_data = splited_voice + \
+                [[pad_value]*chunk_size]*(max_chunk - chunk_num)
         else:
             lengths.append(max_chunk)
             padded_data = splited_voice[:max_chunk]
@@ -283,42 +376,43 @@ def split_voice_with_pad(voice: List[float], chunk_size=1024, pad_value=0.0) -> 
                 length = length + 1
                 sum = sum + abs(voice[idx+i])
         next_avg = sum/length
+        current_chunk['avg'] = current_chunk['sum'] / current_chunk['len']
         return next_avg < current_chunk['avg'] * 0.5 or next_avg > current_chunk['avg'] * 2 or current_chunk['len'] >= chunk_size
 
     def withPads(chunk):
-        if chunk['len'] == chunk_size:
+        chunk_len = chunk['len']
+        if chunk_len == chunk_size:
             return chunk['data']
-        return chunk['data'] + [pad_value] * (chunk_size - chunk['len'])
+        return chunk['data'].tolist() + [pad_value] * (chunk_size - chunk_len)
 
-    current_chunk = None
+    current_chunk = {
+        'start': 0,
+        'end': 0,
+        'sum': 0.0,
+        'len': 0,
+    }
     for i in range(len(voice)):
-        if current_chunk is None:
-            current_chunk = {
-                'start': i,
-                'data': [voice[i]],
-                'sum': voice[i],
-                'len': 1,
-                'avg': voice[i],
-            }
-        elif current_chunk is not None and isGap(current_chunk, i) and current_chunk['len'] > 1000:
+        if current_chunk['len'] > 1000 and isGap(current_chunk, i):
             current_chunk['end'] = i
+            current_chunk['data'] = voice[current_chunk['start']
+                :current_chunk['end']]
             chunks.append(withPads(current_chunk))
             current_chunk = {
                 'start': i,
-                'data': [voice[i]],
+                # 'data': [voice[i]],
                 'sum': voice[i],
                 'len': 1,
-                'avg': voice[i],
             }
-        elif current_chunk is not None:
-            current_chunk['data'].append(voice[i])
+        else:
+            # current_chunk['data'].append(voice[i])
             current_chunk['sum'] = current_chunk['sum'] + abs(voice[i])
             current_chunk['len'] = current_chunk['len'] + 1
-            current_chunk['avg'] = current_chunk['sum'] / current_chunk['len']
+            # current_chunk['avg'] = current_chunk['sum'] / current_chunk['len']
+    if current_chunk['len'] > 500:
+        current_chunk['data'] = voice[current_chunk['start']:]
+        chunks.append(withPads(current_chunk))
 
     return chunks
-# def to_input_tensor(source: np.ndarray, device: torch.device) -> torch.Tensor:
-#     return torch.tensor(source, dtype=torch.float, device=device).transpose(0, 1)
 
 
 def load_voices_with_pad(voice_path, sample_rate, resample_rate=8000, chunk_size=2048, chunk_num=40, pad_value=0.0):
